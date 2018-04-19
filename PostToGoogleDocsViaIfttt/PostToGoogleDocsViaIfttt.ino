0976744a1a7b4adf264e8e6adeea1c2e05f52c99
Rafal Magda <rafalmag@gmail.com>
2018-04-19 20:09:50 +0200

    PostToGoogleDocsViaIfttt

---
+++ PostToGoogleDocsViaIfttt/PostToGoogleDocsViaIfttt.ino
@@ -0,0 +1,137 @@
+/*
+    This sketch sends data via HTTP GET requests to data.sparkfun.com service.
+
+    You need to get streamId and privateKey at data.sparkfun.com and paste them
+    below. Or just customize this script to talk to other HTTP servers.
+
+*/
+
+
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <HTTPClient.h>
+#include "C:\Users\Rafal\Documents\Arduino\myWifi.h"
+
+//const char* ssid     = "your-ssid";
+//const char* password = "your-password";
+//const char* iftttKey = "secret";
+
+// https://maker.ifttt.com/trigger/test/with/key/secret
+const char* host = "maker.ifttt.com";
+const char* eventId   = "test";
+
+// Use web browser to view and copy
+// SHA1 fingerprint of the certificate
+const char* fingerprint = "‎36 4e a6 21 ee 85 70 ad f6 9c 07 a8 2b f6 86 78 d0 15 71 35";
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+int value = 3;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+uint8_t temprature_sens_read();
+#ifdef __cplusplus
+}
+#endif
+uint8_t temprature_sens_read();
+
+float temp() {
+  return (temprature_sens_read() - 32) / 1.8;
+}
+
+void loop()
+{
+  delay(5000);
+  --value;
+
+  if (value < 0) return;
+
+
+  // We now create a URI for the request
+  //  String url = String("https://") + host + "/trigger/" + eventId + "/with/key/" + iftttKey;
+  String uri = String("/trigger/") + eventId + "/with/key/" + iftttKey;
+
+  const char* rootCa = "-----BEGIN CERTIFICATE-----\n" \
+                       "MIIEADCCAuigAwIBAgIBADANBgkqhkiG9w0BAQUFADBjMQswCQYDVQQGEwJVUzEh\n" \
+                       "MB8GA1UEChMYVGhlIEdvIERhZGR5IEdyb3VwLCBJbmMuMTEwLwYDVQQLEyhHbyBE\n" \
+                       "YWRkeSBDbGFzcyAyIENlcnRpZmljYXRpb24gQXV0aG9yaXR5MB4XDTA0MDYyOTE3\n" \
+                       "MDYyMFoXDTM0MDYyOTE3MDYyMFowYzELMAkGA1UEBhMCVVMxITAfBgNVBAoTGFRo\n" \
+                       "ZSBHbyBEYWRkeSBHcm91cCwgSW5jLjExMC8GA1UECxMoR28gRGFkZHkgQ2xhc3Mg\n" \
+                       "MiBDZXJ0aWZpY2F0aW9uIEF1dGhvcml0eTCCASAwDQYJKoZIhvcNAQEBBQADggEN\n" \
+                       "ADCCAQgCggEBAN6d1+pXGEmhW+vXX0iG6r7d/+TvZxz0ZWizV3GgXne77ZtJ6XCA\n" \
+                       "PVYYYwhv2vLM0D9/AlQiVBDYsoHUwHU9S3/Hd8M+eKsaA7Ugay9qK7HFiH7Eux6w\n" \
+                       "wdhFJ2+qN1j3hybX2C32qRe3H3I2TqYXP2WYktsqbl2i/ojgC95/5Y0V4evLOtXi\n" \
+                       "EqITLdiOr18SPaAIBQi2XKVlOARFmR6jYGB0xUGlcmIbYsUfb18aQr4CUWWoriMY\n" \
+                       "avx4A6lNf4DD+qta/KFApMoZFv6yyO9ecw3ud72a9nmYvLEHZ6IVDd2gWMZEewo+\n" \
+                       "YihfukEHU1jPEX44dMX4/7VpkI+EdOqXG68CAQOjgcAwgb0wHQYDVR0OBBYEFNLE\n" \
+                       "sNKR1EwRcbNhyz2h/t2oatTjMIGNBgNVHSMEgYUwgYKAFNLEsNKR1EwRcbNhyz2h\n" \
+                       "/t2oatTjoWekZTBjMQswCQYDVQQGEwJVUzEhMB8GA1UEChMYVGhlIEdvIERhZGR5\n" \
+                       "IEdyb3VwLCBJbmMuMTEwLwYDVQQLEyhHbyBEYWRkeSBDbGFzcyAyIENlcnRpZmlj\n" \
+                       "YXRpb24gQXV0aG9yaXR5ggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQAD\n" \
+                       "ggEBADJL87LKPpH8EsahB4yOd6AzBhRckB4Y9wimPQoZ+YeAEW5p5JYXMP80kWNy\n" \
+                       "OO7MHAGjHZQopDH2esRU1/blMVgDoszOYtuURXO1v0XJJLXVggKtI3lpjbi2Tc7P\n" \
+                       "TMozI+gciKqdi0FuFskg5YmezTvacPd+mSYgFFQlq25zheabIZ0KbIIOqPjCDPoQ\n" \
+                       "HmyW74cNxA9hi63ugyuV+I6ShHI56yDqg+2DzZduCLzrTia2cyvk0/ZM/iZx4mER\n" \
+                       "dEr/VxqHD3VILs9RaRegAhJhldXRQLIQTO7ErBBDpqWeCtWVYpoNz4iCxTIM5Cuf\n" \
+                       "ReYNnyicsbkqWletNw+vHX/bvZ8=\n" \
+                       "-----END CERTIFICATE-----\n";
+
+
+  HTTPClient https;
+  Serial.println("[HTTPS] begin to " + uri);
+  // configure traged server and url
+  https.begin(String(host) + "/", 443, uri, rootCa); //HTTPS
+
+  String data = String("{\"value1\":") + hallRead() + ",\"value2\":" + temp() + ",\"value3\":\"vv31\"}";
+  https.addHeader("Content-Type", "application/json; charset=utf-8");
+  //  https.addHeader("Content-Length", String() + data.length());
+
+  Serial.print("[HTTPS] POST...\n");
+  // start connection and send HTTPs header
+  int httpCode = https.POST(data);
+
+  // httpCode will be negative on error
+  if (httpCode > 0) {
+    // HTTP header has been send and Server response header has been handled
+    Serial.printf("[HTTP] POST... code: %d\n", httpCode);
+
+    // file found at server
+    if (httpCode == HTTP_CODE_OK) {
+      String payload = https.getString();
+      Serial.println(payload);
+    }
+  } else {
+    Serial.printf("[HTTPS] POST... failed, error: %s\n", https.errorToString(httpCode).c_str());
+  }
+
+  https.end();
+
+}
+
+
+
